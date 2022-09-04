7518e2bdc7bb7429176e2390c22d99d3212c54f0
marina zaporozhets <mazavlia@mail.ru>
2022-09-05 00:36:23 +0500

    add Sem2Task9 end

--- Sem2Task9/Program.cs
+++ Sem2Task9/Program.cs
@@ -3,24 +3,85 @@
 // [10, 99] и показывает наибольшую цифру числа.
 //===========================================================================
 
-System.Random numberGenerator = new System.Random();
+void MyVariant()
 
-// Генератор случайных чисел от 10 (включая 10) до 100 (т.е. меньше 100, последнее число - 99)
-int number = numberGenerator.Next(10, 100);
+{
+    Console.WriteLine("Method 1");
+    System.Random numberGenerator = new System.Random();
 
-//Выводим на печать сгенерированное число.
-Console.WriteLine(number);
+    // Генератор случайных чисел от 10 (включая 10) до 100 (т.е. меньше 100, последнее число - 99)
+    int number = numberGenerator.Next(10, 100);
 
-int firstDigit = number / 10;
-int secondDigit = number % 10;
+    //Выводим на печать сгенерированное число.
+    Console.WriteLine(number);
 
-// Выводим на печать максимальное число из 2х значений методом Math.Max.
-Console.WriteLine(Math.Max(firstDigit, secondDigit));
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
 
-// Выводим на печать максимальное число из 2х значений методом тернарного оператора.
-Console.WriteLine((firstDigit > secondDigit) ? firstDigit : secondDigit);
+    // Вариант 1. Сравнение чисел.
+    if (firstDigit > secondDigit)
+    {
+        Console.WriteLine(firstDigit);
+    }
+    else
+    {
+        Console.WriteLine(secondDigit);
+    }
+}
 
-//Вариант char.
-char[] charArray = number.ToString().ToCharArray();
-Console.WriteLine(((int)charArray[0] > (int)charArray[1]) ? charArray[0] : charArray[1]);
+void mathMaxMethod()
 
+{
+    Console.WriteLine("Method 2");
+    // Выводим на печать максимальное число из 2х значений методом Math.Max.
+    System.Random numberGenerator = new System.Random();
+
+    // Генератор случайных чисел от 10 (включая 10) до 100 (т.е. меньше 100, последнее число - 99)
+    int number = numberGenerator.Next(10, 100);
+
+    //Выводим на печать сгенерированное число.
+    Console.WriteLine(number);
+
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    Console.WriteLine(Math.Max(firstDigit, secondDigit));
+}
+
+void ternOperatorMethod()
+
+{
+    Console.WriteLine("Method 3");
+    System.Random numberGenerator = new System.Random();
+
+    // Генератор случайных чисел от 10 (включая 10) до 100 (т.е. меньше 100, последнее число - 99)
+    int number = numberGenerator.Next(10, 100);
+
+    //Выводим на печать сгенерированное число.
+    Console.WriteLine(number);
+
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    // Выводим на печать максимальное число из 2х значений методом тернарного оператора.
+    Console.WriteLine((firstDigit > secondDigit) ? firstDigit : secondDigit);
+}
+
+void charMethod()
+{
+    Console.WriteLine("Method 4");
+    System.Random numberGenerator = new System.Random();
+
+    // Генератор случайных чисел от 10 (включая 10) до 100 (т.е. меньше 100, последнее число - 99)
+    int number = numberGenerator.Next(10, 100);
+
+    //Выводим на печать сгенерированное число.
+    Console.WriteLine(number);
+
+        //Вариант char.
+    char[] charArray = number.ToString().ToCharArray();
+    Console.WriteLine(((int)charArray[0] > (int)charArray[1]) ? charArray[0] : charArray[1]);
+}
+
+MyVariant();
+mathMaxMethod();
+ternOperatorMethod();
+charMethod();
