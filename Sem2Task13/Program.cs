8e37138c23f5f674f607722144a8ccbe3fc5456e
marina zaporozhets <mazavlia@mail.ru>
2022-09-05 03:14:17 +0500

    add hometask 13

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿//===========================================================================
+// № 13 Напишите программу, которая выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет.
+//===========================================================================
+
+void Method1()
+{
+Console.WriteLine("Введите любое число: ");
+string? inputLine = Console.ReadLine();
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+
+    if (inputNumber > 99) 
+    {
+        char[] charArray = inputNumber.ToString().ToCharArray();
+        Console.WriteLine("Третья цифра введенного Вами числа: "+ charArray[2]);
+    }
+    else 
+    {
+        Console.WriteLine("Третьей цифры в введенном Вами числе нет.");
+    }
+}
+}
+
+Method1();
