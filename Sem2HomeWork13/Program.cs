bf5fcfcd31f9acad18a9857ef74560f5057b2715
marina zaporozhets <mazavlia@mail.ru>
2022-09-06 01:06:51 +0500

    Add sem2Hw13

---
+++ Sem2HomeWork13/Program.cs
@@ -0,0 +1,39 @@
+﻿//===========================================================================
+// № 13 Напишите программу, которая выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет.
+//===========================================================================
+
+int inputNumber = 0;
+bool result = false;
+
+// Получаем число от пользователя.
+void ReadData()
+{
+    Console.WriteLine("Введите любое трехзначное число: ");
+    string? inputLine = Console.ReadLine();
+    inputNumber = int.Parse(inputLine);
+}
+
+// Определяем диапазон числа.
+void CalculateData()
+{
+    result = (inputNumber > 99);
+}
+
+// Выводим данные вычисления.
+void PrintData()
+{
+    if (result)
+    {
+        char[] charArray = inputNumber.ToString().ToCharArray();
+        Console.WriteLine("Третья цифра введенного Вами числа: "+ charArray[2]);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры в введенном Вами числе нет.");
+    }
+}
+
+ReadData();
+CalculateData();
+PrintData();
