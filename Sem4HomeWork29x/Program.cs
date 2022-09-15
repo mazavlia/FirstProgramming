ad9efbfed00dc31c18fbccb1d687aa6e7bc2baaf
marina zaporozhets <mazavlia@mail.ru>
2022-09-15 11:21:23 +0500

    add sem4 hw29x

---
+++ Sem4HomeWork29x/Program.cs
@@ -0,0 +1,32 @@
+﻿//====================================================================================
+// Дополнительно: Написать программу которая из имен через запятую выберет случайное
+// имя и выведет в терминал
+// Игорь, Антон, Сергей -> Антон
+//====================================================================================
+
+// Чтение данных из консоли
+string ReadDataString(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Возвращаем значение
+    return Console.ReadLine() ?? "0";
+}
+
+// Выбираем одного из нескольких введённых
+void Choise(string line)
+{
+    string[] peoples = line.Split(", ");
+    Random ren = new Random();
+
+    // for (int i = 0; i < peoples.Length; i++)
+    // {
+    //     Console.Write(peoples[i] + " ");
+    // }
+    Console.Write(peoples[ren.Next(0, peoples.Length)]);
+
+}
+
+string peoples = ReadDataString("Введите людей через запятую: ");
+
+Choise(peoples);
