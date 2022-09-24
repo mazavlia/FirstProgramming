a157da599d319ee0278c36bcc04d2ec682cfe576
marina zaporozhets <mazavlia@mail.ru>
2022-09-25 04:08:51 +0500

    Added task 56, Changed task 54

---
+++ Sem8HomeWork56/Program.cs
@@ -0,0 +1,85 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей 
+// суммой элементов: 1 строка
+
+// Получение данных от пользователя и преобразование в число.
+int ReadData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Универсальный метод генерации и заполнения двумерного массива.
+int[,] Gen2DArray(int countRow, int countColumn, int start, int stop)
+{
+    int[,] array2D = new int[countRow, countColumn];
+    System.Random rnd = new System.Random();
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rnd.Next(start, stop + 1);
+        }
+    }
+    return array2D;
+}
+
+// Печать двумерного массива.
+void Print2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Метод поиска строки с минимальной суммой элементов.
+int FindMinRow(int[,] arr)
+{
+    int min = int.MaxValue;
+    int sum = 0;
+    int outIndexRow = -1;
+    
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sum = sum + arr[i, j];
+        }
+            if (min > sum)
+            {
+                min = sum;
+                outIndexRow = i+1;
+            }
+            sum = 0;
+        }
+return outIndexRow;
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(string prefix, int line)
+{
+    Console.WriteLine(prefix + line);
+}
+
+// Описание программы.
+int row = ReadData("Введите количество строк: ");
+int column = ReadData("Введите количество столбцов: ");
+
+int[,] arr2D = Gen2DArray(row, column, 0, 99);
+Print2DArray(arr2D);
+
+PrintResult("Минимальную сумму элементов массива содержит строка: ", FindMinRow(arr2D));
+
