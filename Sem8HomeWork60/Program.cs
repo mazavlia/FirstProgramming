a048ad71861abf8346e99e8b22a5c2b90190f520
marina zaporozhets <mazavlia@mail.ru>
2022-09-25 18:44:40 +0500

    Added Task60

---
+++ Sem8HomeWork60/Program.cs
@@ -0,0 +1,76 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, 
+// которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+// Получение данных от пользователя и преобразование в число.
+int ReadData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Генерация 3D-массива.
+int[,,] Gen3DArray(int[] sizes, int min, int max)
+{
+    int[,,] result = new int[sizes[0], sizes[1], sizes[2]];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            int k = 0;
+            while (k < result.GetLength(2))
+            {
+                int element = new Random().Next(min, max + 1);
+                if (FindElement(result, element)) continue;
+                result[i, j, k] = element;
+                k++;
+            }
+        }
+    }
+    return result;
+}
+
+// Проверка на повторяющиеся элементы.
+bool FindElement(int[,,] array, int elem)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                if (array[i, j, k] == elem) return true;
+            }
+        }
+    }
+    return false;
+}
+
+// Печать трехмерного массива.
+void Print3DArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i},{j},{k}) ");
+            }
+            Console.WriteLine();           
+        }
+    }
+}
+  
+// Описание программы.  
+int num1 = ReadData("Введите первую размерность трехмерного массива: ");
+int num2 = ReadData("Введите вторую размерность трехмерного массива: ");
+int num3 = ReadData("Введите третью размерность трехмерного массива: ");
+
+int[,,] array3D = Gen3DArray(new int[] { num1, num2, num3, }, 10, 99);
+Print3DArray(array3D);
