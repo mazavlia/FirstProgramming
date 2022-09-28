64e6fce09946f7da874cb6caac2c9bb5b01d99eb
marina zaporozhets <mazavlia@mail.ru>
2022-09-29 03:56:39 +0500

    Addad tasks 65, 66

---
+++ Sem9HomeWork65/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от M до N.
+
+// Чтение данных из консоли.
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+// Печать результата.
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+
+// Cтрока натуральных чисел от M до N.
+string RecMN(int numM, int numN)
+{    
+if (numM >= numN) return numN.ToString(); 
+return numM + " " + RecMN(numM + 1, numN);    
+}
+
+// Описание программы.
+int M = ReadData("Введите число M: ");
+int N = ReadData("Введите число N: ");
+
+PrintResult("Строка значений в промежутке от M до N: " + (M > N ? RecMN(N, M) : RecMN(M, N)));
---
+++ Sem9HomeWork66/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+//Сумма натуральных чисел от М до N.
+int RecSumMN(int numM, int numN)
+{    
+int sum = 0;
+if (numM >= numN) return numN; 
+sum = numM + RecSumMN(numM + 1, numN); 
+return sum;
+}
+
+int M = ReadData("Введите число M: ");
+int N = ReadData("Введите число N: ");
+
+PrintResult("Сумма значений от M до N: " + (M > N ? RecSumMN(N, M) : RecSumMN(M, N)));
